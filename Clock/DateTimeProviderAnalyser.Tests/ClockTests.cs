176e16cf8321bc61ba7d4bf32f529573499f4c31
Michal Motyčka <michal.motycka@riganti.cz>
2019-10-31 00:17:24 +0100

    Added analyser.

---
+++ Clock/DateTimeProviderAnalyser.Tests/ClockTests.cs
@@ -0,0 +1,86 @@
+﻿using DateTimeProviderAnalyser.DateTimeOffsetNow;
+using DateTimeProviderAnalyser.Tests.TestHelpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace DateTimeProviderAnalyser.Tests
+{
+    public class ClockTests : CodeFixVerifier
+    {
+        private const string SourceCodeWithIssue = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public TypeName()
+            {
+                var now = DateTimeOffset.Now;
+            }
+        }
+    }";
+
+        private const string SourceCodeWithFix = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public TypeName()
+            {
+                var now = DateTimeProvider.Now;
+            }
+        }
+    }";
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new DateTimeOffsetNowCodeFix();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DateTimeOffsetNowAnalyser();
+        }
+
+        [Fact]
+        public void ExpectNoDiagnosticResults()
+        {
+            const string source = @"";
+            VerifyCSharpDiagnostic(source);
+        }
+
+        [Fact]
+        public void IdentifySuggestedFix()
+        {
+            var expected = new DiagnosticResult
+            {
+                Id = "DateTimeOffsetNowAnalyser",
+                Message = "Use DateTimeProvider.Now instead of DateTimeOffset.Now",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] {new DiagnosticResultLocation("Test0.cs", 10, 27)}
+            };
+
+            VerifyCSharpDiagnostic(SourceCodeWithIssue, expected);
+        }
+
+        [Fact]
+        public void ApplySuggestedFix()
+        {
+            var expected = new DiagnosticResult
+            {
+                Id = "DateTimeOffsetNowAnalyser",
+                Message = "Use DateTimeProvider.Now instead of DateTimeOffset.Now",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] {new DiagnosticResultLocation("Test0.cs", 10, 27)}
+            };
+
+            VerifyCSharpDiagnostic(SourceCodeWithIssue, expected);
+            VerifyCSharpFix(SourceCodeWithIssue, SourceCodeWithFix, null, true);
+        }
+    }
+}
---
+++ Clock/Tests/ClockAnalyserTests.cs
@@ -0,0 +1,86 @@
+﻿using DateTimeProviderAnalyser.DateTimeNow;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Tests.TestHelpers;
+using Xunit;
+
+namespace Tests
+{
+    public class ClockAnalyserTests : CodeFixVerifier
+    {
+        private const string SourceCodeWithIssue = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public TypeName()
+            {
+                var now = DateTimeOffset.Now;
+            }
+        }
+    }";
+
+        private const string SourceCodeWithFix = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public TypeName()
+            {
+                var now = DateTimeProvider.Now;
+            }
+        }
+    }";
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new ClockNowCodeFix();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ClockNowAnalyser();
+        }
+
+        [Fact]
+        public void ExpectNoDiagnosticResults()
+        {
+            const string source = @"";
+            VerifyCSharpDiagnostic(source);
+        }
+
+        [Fact]
+        public void IdentifySuggestedFix()
+        {
+            var expected = new DiagnosticResult
+            {
+                Id = "ClockNowAnalyser",
+                Message = "Use Clock.UtcNow instead of DateTime",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] {new DiagnosticResultLocation("Test0.cs", 10, 27)}
+            };
+
+            VerifyCSharpDiagnostic(SourceCodeWithIssue, expected);
+        }
+
+        [Fact]
+        public void ApplySuggestedFix()
+        {
+            var expected = new DiagnosticResult
+            {
+                Id = "ClockNowAnalyser",
+                Message = "Use Clock.UtcNow instead of DateTime",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] {new DiagnosticResultLocation("Test0.cs", 10, 27)}
+            };
+
+            VerifyCSharpDiagnostic(SourceCodeWithIssue, expected);
+            VerifyCSharpFix(SourceCodeWithIssue, SourceCodeWithFix, null, true);
+        }
+    }
+}
